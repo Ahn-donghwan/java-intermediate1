0ce063796156cd0b4224fa97d0dda7cb92c56c94
안동환 <ddh1713flrwls1713@gmail.com>
2024-12-18 15:31:30 +0900

    first commit

---
+++ src/main/java/com/sparta/javaintermediate1/JavaIntermediate1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.javaintermediate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIntermediate1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaIntermediate1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/javaintermediate1/JavaIntermediate1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.javaintermediate1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIntermediate1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
